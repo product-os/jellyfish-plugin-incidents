903004948cd51f7f71681b86afae7376c96fe7da
Josh Bowling <josh@balena.io>
2022-07-29 07:15:55 +0000

    Handle component update webhooks
    Create incident contracts from incoming Statuspage component webhooks
    
    Change-type: minor
    Signed-off-by: Josh Bowling <josh@monarci.com>

--- lib/actions/action-integration-statuspage-import-incident.ts
+++ lib/actions/action-integration-statuspage-import-incident.ts
@@ -1,16 +1,41 @@
-import { defaultEnvironment } from '@balena/jellyfish-environment';
-import type { ActionDefinition, WorkerContext } from '@balena/jellyfish-worker';
-import { getLogger } from '@balena/jellyfish-logger';
-import type { TypeContract } from 'autumndb';
-import axios from 'axios';
+import { defaultEnvironment as environment } from '@balena/jellyfish-environment';
+import type {
+	ActionDefinition,
+	ActionHandlerRequest,
+	WorkerContext,
+} from '@balena/jellyfish-worker';
+import { strict as assert } from 'assert';
+import type { AutumnDBSession, ContractSummary, TypeContract } from 'autumndb';
+import axios, { AxiosResponse } from 'axios';
 import type { Operation } from 'fast-json-patch';
 import _ from 'lodash';
 import { v4 as uuidv4 } from 'uuid';
 import { statusOptions } from '../contracts/incident';
 import type { IncidentContract, StatuspageContract } from '../types';
 
-const logger = getLogger(__filename);
 export const STATUSPAGE_ENDPOINT = 'https://api.statuspage.io/v1';
+
+interface UpdatePayload {
+	page: {
+		id: string;
+		status_description: string;
+		[k: string]: string;
+	};
+	incident?: {
+		status: string;
+		id: string;
+		impact: string;
+		name: string;
+		[k: string]: string;
+	};
+	component?: {
+		status: string;
+		id: string;
+		name: string;
+		[k: string]: string;
+	};
+	[k: string]: unknown;
+}
 
 async function getByMirrorId(context: WorkerContext, type: string, id: string) {
 	const [result] = await context.query(
@@ -45,162 +70,182 @@ async function getByMirrorId(context: WorkerContext, type: string, id: string) {
 	return result;
 }
 
-// TODO: Expand payload definition to handle more information
-interface IncidentUpdatePayload {
-	page: {
-		id: string;
-		status_description: string;
-		[k: string]: string;
-	};
-	incident: {
-		status: string;
-		id: string;
-		impact: string;
-		name: string;
-		[k: string]: string;
-	};
-	[k: string]: unknown;
+async function httpStatuspage(
+	token: string,
+	path: string,
+): Promise<AxiosResponse> {
+	return axios({
+		method: 'GET',
+		url: `${STATUSPAGE_ENDPOINT}${path}`,
+		headers: {
+			Authorization: `OAuth ${token}`,
+		},
+	});
 }
 
-const handler: ActionDefinition['handler'] = async (
-	session,
-	context,
-	contract,
-	request,
-) => {
-	// Validate webhook Statuspage page ID
-	const payload = contract.data.payload as IncidentUpdatePayload;
-	if (!defaultEnvironment.integration.statuspage.pages[payload.page.id]) {
-		logger.warn(request.logContext, 'Webhook from unknown Statuspage', {
-			pageId: payload.page.id,
-		});
-		return {
-			id: contract.id,
-			type: contract.type,
-			version: contract.version,
-			slug: contract.slug,
-		};
+/**
+ * Decide internal incident status based on external status
+ *
+ * @param externalStatus - external incident status
+ * @returns internal status string
+ */
+function getStatus(externalStatus: string): string {
+	if (externalStatus === 'operational') {
+		return 'resolved';
+	}
+	return statusOptions.includes(externalStatus) ? externalStatus : 'open';
+}
+
+/**
+ * Validate incoming Statuspage incident webhook payloads
+ *
+ * @param payload - parsed webhook payload
+ */
+async function validateIncidentWebhook(payload: UpdatePayload): Promise<void> {
+	if (!payload.incident) {
+		return;
+	}
+	const response = await httpStatuspage(
+		environment.integration.statuspage.pages[payload.page.id],
+		`/pages/${payload.page.id}/incidents/${payload.incident.id}`,
+	);
+	assert(
+		response.data.status === payload.incident.status &&
+			response.data.impact === payload.incident.impact &&
+			response.data.name === payload.incident.name,
+		new Error(
+			`Statuspage incident mismatch: ${JSON.stringify(
+				{
+					page: payload.page.id,
+					incident: payload.incident.id,
+					status: payload.incident.status,
+					remoteStatus: response.data.status,
+					impact: payload.incident.impact,
+					remoteImpact: response.data.impact,
+					name: payload.incident.name,
+					remoteName: response.data.name,
+				},
+				null,
+				2,
+			)}`,
+		),
+	);
+}
+
+/**
+ * Validate incoming Statuspage component webhook payloads
+ *
+ * @param payload - parsed webhook payload
+ */
+async function validateComponentWebhook(payload: UpdatePayload): Promise<void> {
+	if (!payload.component) {
+		return;
+	}
+	const response = await httpStatuspage(
+		environment.integration.statuspage.pages[payload.page.id],
+		`/pages/${payload.page.id}/components/${payload.component.id}`,
+	);
+	assert(
+		response.data.status === payload.component.status &&
+			response.data.name === payload.component.name,
+		new Error(
+			`Statuspage component mismatch: ${payload.page.id}, ${payload.component.id}`,
+		),
+	);
+}
+
+async function initIncident(
+	context: WorkerContext,
+	session: AutumnDBSession,
+	request: ActionHandlerRequest,
+	statuspage: StatuspageContract,
+	incident: IncidentContract,
+): Promise<void> {
+	// Link incident to statuspage
+	await context.insertCard(
+		session,
+		context.cards['link@1.0.0'] as TypeContract,
+		{
+			timestamp: request.timestamp,
+			attachEvents: false,
+		},
+		{
+			slug: `link-statuspage-${statuspage.id}-has-attached-${incident.slug}`,
+			name: 'has attached',
+			data: {
+				inverseName: 'is attached to',
+				from: {
+					id: statuspage.id,
+					type: statuspage.type,
+				},
+				to: {
+					id: incident.id,
+					type: incident.type,
+				},
+			},
+		},
+	);
+
+	// Actor to create ping message with
+	let actor = request.actor;
+	const hubot = await context.getCardBySlug(
+		context.privilegedSession,
+		'user-hubot@1.0.0',
+	);
+	if (hubot) {
+		actor = hubot.id;
 	}
 
-	// Get statuspage contract
-	const statuspageMirrorId = `${STATUSPAGE_ENDPOINT}/pages/${payload.page.id}`;
-	let statuspage = (await getByMirrorId(
-		context,
-		'statuspage',
-		statuspageMirrorId,
-	)) as StatuspageContract;
-
-	// Create statuspage contract if it doesn't exist
-	if (!statuspage) {
-		try {
-			const remoteStatuspage = await axios({
-				method: 'GET',
-				url: `${STATUSPAGE_ENDPOINT}/pages/${payload.page.id}`,
-				headers: {
-					Authorization: `OAuth ${
-						defaultEnvironment.integration.statuspage.pages[payload.page.id]
-					}`,
+	// Add message with ping to incident timeline
+	await context.insertCard(
+		session,
+		context.cards['action-request@1.0.0'] as TypeContract,
+		{
+			timestamp: request.timestamp,
+			attachEvents: false,
+		},
+		{
+			data: {
+				actor,
+				context: request.logContext,
+				action: 'action-create-event@1.0.0',
+				card: incident.id,
+				type: incident.type,
+				epoch: new Date().valueOf(),
+				timestamp: request.timestamp,
+				input: {
+					id: incident.id,
 				},
-			});
-			statuspage = (await context.insertCard(
-				session,
-				context.cards['statuspage@1.0.0'] as TypeContract,
-				{
-					timestamp: request.timestamp,
-					attachEvents: true,
-					actor: request.actor,
-				},
-				{
-					slug: `statuspage-${payload.page.id}`,
-					data: {
-						name: remoteStatuspage.data.name || '',
-						description: remoteStatuspage.data.page_description || '',
-						domain: remoteStatuspage.data.domain || '',
-						subdomain: remoteStatuspage.data.subdomain || '',
-						mirrors: [statuspageMirrorId],
+				arguments: {
+					type: 'message',
+					payload: {
+						message: '@@reliability New incident from Statuspage',
 					},
 				},
-			)) as StatuspageContract;
-		} catch (error: any) {
-			logger.warn(request.logContext, 'Failed to get Statuspage details', {
-				pageId: payload.page.id,
-				incidentId: payload.incident.id,
-				error: {
-					code: error.code,
-					message: error.data.message,
-					status: error.response.status,
-					statusText: error.response.statusText,
-				},
-			});
-			return {
-				id: contract.id,
-				type: contract.type,
-				version: contract.version,
-				slug: contract.slug,
-			};
-		}
-	}
-
-	// Validate webhook incident ID and status
-	try {
-		const response = await axios({
-			method: 'GET',
-			url: `${STATUSPAGE_ENDPOINT}/pages/${payload.page.id}/incidents/${payload.incident.id}`,
-			headers: {
-				Authorization: `OAuth ${
-					defaultEnvironment.integration.statuspage.pages[payload.page.id]
-				}`,
 			},
-		});
-		if (
-			response.data.status !== payload.incident.status ||
-			response.data.impact !== payload.incident.impact ||
-			response.data.name !== payload.incident.name
-		) {
-			logger.warn(request.logContext, 'Statuspage incident mismatch', {
-				pageId: payload.page.id,
-				incidentId: payload.incident.id,
-				remoteStatus: response.data.status,
-				webhookStatus: payload.incident.status,
-				remoteImpact: response.data.impact,
-				webhookImpact: payload.incident.impact,
-				remoteName: response.data.name,
-				webhookName: payload.incident.name,
-			});
-			return {
-				id: contract.id,
-				type: contract.type,
-				version: contract.version,
-				slug: contract.slug,
-			};
-		}
-	} catch (error: any) {
-		logger.warn(
-			request.logContext,
-			'Failed to get Statuspage incident details',
-			{
-				pageId: payload.page.id,
-				incidentId: payload.incident.id,
-				error: {
-					code: error.code,
-					message: error.data.message,
-					status: error.response.status,
-					statusText: error.response.statusText,
-				},
-			},
-		);
-		return {
-			id: contract.id,
-			type: contract.type,
-			version: contract.version,
-			slug: contract.slug,
-		};
-	}
+		},
+	);
+}
 
-	// Upsert incident contract
-	const status = statusOptions.includes(payload.incident.status)
-		? payload.incident.status
-		: 'open';
+/**
+ * Create incident contract from incident update webhook payload
+ *
+ * @param context - worker context
+ * @param session - execution session
+ * @param request - action handler request
+ * @param payload - parsed webhook payload
+ * @param statuspage - statuspage contract
+ * @returns incident contract summary
+ */
+async function fromIncidentPayload(
+	context: WorkerContext,
+	session: AutumnDBSession,
+	request: ActionHandlerRequest,
+	payload: UpdatePayload,
+	statuspage: StatuspageContract,
+): Promise<ContractSummary> {
+	assert(payload.incident);
+	const status = getStatus(payload.incident.status);
 	const incidentMirrorId = `${STATUSPAGE_ENDPOINT}/pages/${payload.page.id}/incidents/${payload.incident.id}`;
 	let incident = (await getByMirrorId(
 		context,
@@ -243,7 +288,6 @@ const handler: ActionDefinition['handler'] = async (
 		);
 	} else {
 		// Create new incident
-		const incidentSlug = `incident-${uuidv4()}`;
 		incident = (await context.insertCard(
 			session,
 			context.cards['incident@1.0.0'] as TypeContract,
@@ -263,71 +307,7 @@ const handler: ActionDefinition['handler'] = async (
 				},
 			},
 		)) as IncidentContract;
-
-		// Link incident to statuspage
-		await context.insertCard(
-			session,
-			context.cards['link@1.0.0'] as TypeContract,
-			{
-				timestamp: request.timestamp,
-				attachEvents: false,
-			},
-			{
-				slug: `link-statuspage-${payload.page.id}-has-attached-${incidentSlug}`,
-				name: 'has attached',
-				data: {
-					inverseName: 'is attached to',
-					from: {
-						id: statuspage.id,
-						type: statuspage.type,
-					},
-					to: {
-						id: incident.id,
-						type: incident.type,
-					},
-				},
-			},
-		);
-
-		// Actor to create ping message with
-		let actor = request.actor;
-		const hubot = await context.getCardBySlug(
-			context.privilegedSession,
-			'user-hubot@1.0.0',
-		);
-		if (hubot) {
-			actor = hubot.id;
-		}
-
-		// Add message with ping to incident timeline
-		await context.insertCard(
-			session,
-			context.cards['action-request@1.0.0'] as TypeContract,
-			{
-				timestamp: request.timestamp,
-				attachEvents: false,
-			},
-			{
-				data: {
-					actor,
-					context: request.logContext,
-					action: 'action-create-event@1.0.0',
-					card: incident.id,
-					type: incident.type,
-					epoch: new Date().valueOf(),
-					timestamp: request.timestamp,
-					input: {
-						id: incident.id,
-					},
-					arguments: {
-						type: 'message',
-						payload: {
-							message: '@@reliability New incident from Statuspage',
-						},
-					},
-				},
-			},
-		);
+		await initIncident(context, session, request, statuspage, incident);
 	}
 
 	return {
@@ -336,6 +316,152 @@ const handler: ActionDefinition['handler'] = async (
 		version: incident.version,
 		slug: incident.slug,
 	};
+}
+
+/**
+ * Create incident contract from component update webhook payload
+ *
+ * @param context - worker context
+ * @param session - execution session
+ * @param request - action handler request
+ * @param payload - parsed webhook payload
+ * @param statuspage - statuspage contract
+ * @returns incident contract summary
+ */
+async function fromComponentPayload(
+	context: WorkerContext,
+	session: AutumnDBSession,
+	request: ActionHandlerRequest,
+	payload: UpdatePayload,
+	statuspage: StatuspageContract,
+): Promise<ContractSummary> {
+	assert(payload.component);
+	const status = getStatus(payload.component.status);
+	const incidentMirrorId = `${STATUSPAGE_ENDPOINT}/pages/${payload.page.id}/components/${payload.component.id}`;
+	let incident = (await getByMirrorId(
+		context,
+		'incident',
+		incidentMirrorId,
+	)) as IncidentContract;
+	if (incident) {
+		const patch: Operation[] = [];
+		if (incident.data.status !== status) {
+			patch.push({
+				op: 'replace',
+				path: '/data/status',
+				value: status,
+			});
+		}
+		if (incident.name !== payload.component.name) {
+			patch.push({
+				op: 'replace',
+				path: '/name',
+				value: payload.component.name,
+			});
+		}
+		await context.patchCard(
+			session,
+			context.cards['incident@1.0.0'] as TypeContract,
+			{
+				timestamp: request.timestamp,
+				attachEvents: true,
+				actor: request.actor,
+			},
+			incident,
+			patch,
+		);
+	} else {
+		// Create new incident
+		incident = (await context.insertCard(
+			session,
+			context.cards['incident@1.0.0'] as TypeContract,
+			{
+				timestamp: request.timestamp,
+				attachEvents: true,
+				actor: request.actor,
+			},
+			{
+				name: payload.component.name || '',
+				slug: `incident-${uuidv4()}`,
+				data: {
+					description: payload.page.status_description,
+					status,
+					mirrors: [incidentMirrorId],
+				},
+			},
+		)) as IncidentContract;
+		await initIncident(context, session, request, statuspage, incident);
+	}
+
+	return {
+		id: incident.id,
+		type: incident.type,
+		version: incident.version,
+		slug: incident.slug,
+	};
+}
+
+const handler: ActionDefinition['handler'] = async (
+	session,
+	context,
+	contract,
+	request,
+) => {
+	// Validate webhook Statuspage page ID
+	const payload = contract.data.payload as UpdatePayload;
+	const token = environment.integration.statuspage.pages[payload.page.id];
+	assert(
+		token,
+		new Error(`Webhook from unknown Statuspage: ${payload.page.id}`),
+	);
+
+	// Try to get statuspage contract
+	const statuspageMirrorId = `${STATUSPAGE_ENDPOINT}/pages/${payload.page.id}`;
+	let statuspage = (await getByMirrorId(
+		context,
+		'statuspage',
+		statuspageMirrorId,
+	)) as StatuspageContract;
+
+	// Create statuspage contract if it doesn't exist
+	if (!statuspage) {
+		const remoteStatuspage = await httpStatuspage(
+			token,
+			`/pages/${payload.page.id}`,
+		);
+		statuspage = (await context.insertCard(
+			session,
+			context.cards['statuspage@1.0.0'] as TypeContract,
+			{
+				timestamp: request.timestamp,
+				attachEvents: true,
+				actor: request.actor,
+			},
+			{
+				slug: `statuspage-${payload.page.id}`,
+				data: {
+					name: remoteStatuspage.data.name || '',
+					description: remoteStatuspage.data.page_description || '',
+					domain: remoteStatuspage.data.domain || '',
+					subdomain: remoteStatuspage.data.subdomain || '',
+					mirrors: [statuspageMirrorId],
+				},
+			},
+		)) as StatuspageContract;
+	}
+
+	// Validate webhook
+	await Promise.all([
+		await validateIncidentWebhook(payload),
+		await validateComponentWebhook(payload),
+	]);
+
+	// Upsert incident contract
+	if (payload.incident) {
+		return fromIncidentPayload(context, session, request, payload, statuspage);
+	} else {
+		return fromComponentPayload(context, session, request, payload, statuspage);
+	}
 };
 
 export const actionIntegrationStatuspageImportIncident: ActionDefinition = {

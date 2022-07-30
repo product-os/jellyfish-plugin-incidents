edb778fe7f1fad1b0930a15341b4d1a64026b7f8
Josh Bowling <josh@balena.io>
2022-07-30 14:39:20 +0900

    Add more tests
    Change-type: patch
    Signed-off-by: Josh Bowling <josh@monarci.com>

--- test/unit/actions/action-integration-statuspage-import-incident.spec.ts
+++ test/unit/actions/action-integration-statuspage-import-incident.spec.ts
@@ -1,4 +1,32 @@
-import { getStatus } from '../../../lib/actions/action-integration-statuspage-import-incident';
+import { testUtils } from 'autumndb';
+import nock from 'nock';
+import {
+	getStatus,
+	STATUSPAGE_ENDPOINT,
+	validateComponentWebhook,
+	validateIncidentWebhook,
+} from '../../../lib/actions/action-integration-statuspage-import-incident';
+
+const mock = {
+	page: {
+		id: 'foobar',
+		name: 'ProductOS',
+		subdomain: 'productos',
+		domain: 'status.jel.ly.fish',
+		status_description: 'buzbaz',
+	},
+	incident: {
+		id: testUtils.generateRandomId().split('-')[0],
+		status: 'major_outage',
+		impact: 'major',
+		name: 'ProductOS outage',
+	},
+	component: {
+		id: testUtils.generateRandomId().split('-')[0],
+		status: 'degraded_performance',
+		name: 'Upstream Hosting Provider - Heroku',
+	},
+};
 
 describe('getStatus()', () => {
 	test('should return "open" on unmatched external status', async () => {
@@ -11,5 +39,128 @@ describe('getStatus()', () => {
 		const externalStatus = 'resolved';
 		const result = getStatus(externalStatus);
 		expect(result).toEqual('resolved');
+	});
+});
+
+describe('validateComponentWebhook()', () => {
+	beforeAll(() => {
+		// Nock page and incident details
+		nock.cleanAll();
+		nock(STATUSPAGE_ENDPOINT)
+			.get(`/pages/${mock.page.id}`)
+			.reply(200, mock.page)
+			.get(`/pages/${mock.page.id}/components/${mock.component.id}`)
+			.reply(200, mock.component);
+	});
+
+	afterEach(() => {
+		nock.cleanAll();
+	});
+
+	test('should not throw on valid component webhook', async () => {
+		await validateComponentWebhook({
+			page: {
+				id: mock.page.id,
+				status_description: mock.page.status_description,
+			},
+			component: {
+				id: mock.component.id,
+				status: mock.component.status,
+				name: mock.component.name,
+			},
+		});
+	});
+
+	test('should not throw on incident webhook', async () => {
+		await validateComponentWebhook({
+			page: {
+				id: mock.page.id,
+				status_description: mock.page.status_description,
+			},
+			incident: {
+				id: mock.incident.id,
+				status: mock.incident.status,
+				impact: mock.incident.impact,
+				name: mock.incident.name,
+			},
+		});
+	});
+
+	test('should throw on invalid incident webhook', async () => {
+		await expect(
+			validateComponentWebhook({
+				page: {
+					id: mock.page.id,
+					status_description: mock.page.status_description,
+				},
+				component: {
+					id: mock.incident.id,
+					status: 'foobar',
+					name: mock.incident.name,
+				},
+			}),
+		).rejects.toThrow();
+	});
+});
+
+describe('validateIncidentWebhook()', () => {
+	beforeAll(() => {
+		// Nock page and incident details
+		nock.cleanAll();
+		nock(STATUSPAGE_ENDPOINT)
+			.get(`/pages/${mock.page.id}`)
+			.reply(200, mock.page)
+			.get(`/pages/${mock.page.id}/incidents/${mock.incident.id}`)
+			.reply(200, mock.incident);
+	});
+
+	afterEach(() => {
+		nock.cleanAll();
+	});
+
+	test('should not throw on valid incident webhook', async () => {
+		await validateIncidentWebhook({
+			page: {
+				id: mock.page.id,
+				status_description: mock.page.status_description,
+			},
+			incident: {
+				id: mock.incident.id,
+				status: mock.incident.status,
+				impact: mock.incident.impact,
+				name: mock.incident.name,
+			},
+		});
+	});
+
+	test('should not throw on component webhook', async () => {
+		await validateIncidentWebhook({
+			page: {
+				id: mock.page.id,
+				status_description: mock.page.status_description,
+			},
+			component: {
+				id: mock.component.id,
+				status: mock.component.status,
+				name: mock.component.name,
+			},
+		});
+	});
+
+	test('should throw on invalid incident webhook', async () => {
+		await expect(
+			validateIncidentWebhook({
+				page: {
+					id: mock.page.id,
+					status_description: mock.page.status_description,
+				},
+				incident: {
+					id: mock.incident.id,
+					status: 'foobar',
+					impact: mock.incident.impact,
+					name: mock.incident.name,
+				},
+			}),
+		).rejects.toThrow();
 	});
 });
